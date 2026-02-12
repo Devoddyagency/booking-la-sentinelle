b2f0675c3315fbcbebb1785fb4154b83c55bbc6c
Nabil_er_rais <dev@oddy.ca>
2026-02-12 12:11:02 +0100

    update migration.sql

--- packages/prisma/migrations/20211105200545_availability_start_and_end_time_as_time/migration.sql
+++ packages/prisma/migrations/20211105200545_availability_start_and_end_time_as_time/migration.sql
@@ -2,11 +2,11 @@
 
 ALTER TABLE "Availability" RENAME COLUMN "startTime" to "old_startTime";
 ALTER TABLE "Availability" RENAME COLUMN "endTime" to "old_endTime";
-ALTER TABLE "Availability" ADD COLUMN "startTime" TIME;
-ALTER TABLE "Availability" ADD COLUMN "endTime" TIME;
+ALTER TABLE "Availability" ADD COLUMN "startTime" TIMESTAMPTZ;
+ALTER TABLE "Availability" ADD COLUMN "endTime" TIMESTAMPTZ;
 
-UPDATE "Availability" SET "startTime" = CAST(CONCAT(CAST(("old_startTime" / 60) AS text), ':00') AS time);
-UPDATE "Availability" SET "endTime" = CAST(CONCAT(CAST(("old_endTime" / 60) AS text), ':00') AS time);
+UPDATE "Availability" SET "startTime" = to_timestamp(0) + (CAST("old_startTime" AS text) || ' minutes')::interval;
+UPDATE "Availability" SET "endTime" = to_timestamp(0) + (CAST("old_endTime" AS text) || ' minutes')::interval;
 
 ALTER TABLE "Availability" DROP COLUMN "old_startTime";
 ALTER TABLE "Availability" DROP COLUMN "old_endTime";
